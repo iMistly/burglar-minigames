67880957bb03ac2ab68442c16a851bc95cd752cf
MistlyH <craftyminer22@gmail.com>
2025-02-11 21:36:03 -0600

    keypad guessing game

--- Mini-Games/keypad/button.gd
+++ Mini-Games/keypad/button.gd
@@ -3,18 +3,29 @@ extends Control
 signal click
 
 @export var INDEX:int
-@export var SPEED:float = 1.0
-
-const BLANK = preload("res://Mini-Games/keypad/tmpTextures/blank.png")
+@export var SPEED:float = 0.25
 
 @onready var texture_rect: TextureRect = $TextureRect
 
-var defaultxt__tecolor
+var default_text_color
+@export var correct_text_color: Color = Color(0.2, 1, 0.2)
+@export var incorrect_text_color: Color = Color(1, 0.2, 0.2)
+
+var active_blinks: int = 0
 
 func _ready():
 	$Label.text = str(INDEX)
-	default_text_color = $Label.font_color
-
+	default_text_color = $Label.get_theme_color("font_color")
+	
+func blink(correct:bool = false):
+	active_blinks += 1
+	$Label.add_theme_color_override("font_color", correct_text_color if correct else incorrect_text_color)
+	await get_tree().create_timer(SPEED).timeout
+	active_blinks -= 1
+	if active_blinks == 0:
+		$Label.add_theme_color_override("font_color", default_text_color)
+	
 func _on_gui_input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		click.emit(self)
+		$AudioStreamPlayer.play()
--- Mini-Games/keypad/handler.gd
+++ Mini-Games/keypad/handler.gd
@@ -1,19 +1,32 @@
 extends Node2D
 
+signal game_results
+
+@export var CODE_LENGTH:int = 4
+
 const BUTTON = preload("res://Mini-Games/keypad/button.tscn")
-@onready var grid_container: GridContainer = $GridContainer
+@onready var grid_container: GridContainer = $keypad
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var code_label: Label = $sticky_note/code
+
+var code:Array
+var disabled_code_num_index:int
+var button_array:Array = []
+
+var game_over: bool = false
+var player_current_guess: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	grid_container.size = Vector2(32*3, 32*4)
 	print(grid_container.size)
-	camera_2d.position = grid_container.position + grid_container.size / 2
+	camera_2d.position = grid_container.position + Vector2(grid_container.size.x, grid_container.size.y / 2)
 	# Buttons 1-9
 	for i in range(1, 10):
 		var button = BUTTON.instantiate()
 		button.INDEX = i
 		button.click.connect(_on_Button_click)
+		button_array.append(button)
 		grid_container.add_child(button)
 	# Filler non-interactable rect
 	var rect = ColorRect.new()
@@ -24,11 +37,31 @@ func _ready() -> void:
 	var button = BUTTON.instantiate()
 	button.INDEX = 0
 	button.click.connect(_on_Button_click)
+	button_array.append(button)
 	grid_container.add_child(button)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Generate code
+	for i in range(CODE_LENGTH):
+		code.append(randi() % 10)
+	disabled_code_num_index = randi() % CODE_LENGTH
+	var text = ""
+	for i in range(CODE_LENGTH):
+		if i == disabled_code_num_index:
+			text += "x"
+		else:
+			text += str(code[i])
+	code_label.text = text
 
 func _on_Button_click(button):
-	print(button.INDEX)
+	if game_over:
+		return
+	if button.INDEX == code[player_current_guess.size()]:
+		player_current_guess.append(button.INDEX)
+		button.blink(true)
+		if player_current_guess.size() == CODE_LENGTH:
+			if player_current_guess == code:
+				print("You win")
+				game_over = true
+	else:
+		player_current_guess.clear()
+		for item in button_array:
+			item.blink(false)
